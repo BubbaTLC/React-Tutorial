64447b0a2ffeec8374fdc5a895938ba8e3d3ba78
BubbaTLC <chabot.tristan@gmail.com>
2021-07-03 14:39:22 -0600

    Completed video 12 - Destructuring props and state. --BC

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/Counter";
-// import Greet from "./components/Greet";
-// import Welcome from "./components/Welcome";
+// import Counter from "./components/Counter";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 // import Hello from "./components/Hello";
 // import Message from "./components/Message"
 
 function App() {
     return (
         <div className="App">
-            <Counter></Counter>
+            {/* <Counter></Counter> */}
             {/* <Message></Message> */}
             {/* <Greet name="Bubba" heroName="Wizard">
                 <p>This is children props</p>
-            </Greet>
-            <Greet name="Aaron" heroName="Cool">
-                <button>Action</button>
-            </Greet>
+            </Greet> */}
+            {/* <Greet name="Aaron" heroName="Cool"> */}
+            {/* <button>Action</button> */}
+            {/* </Greet> */}
             <Greet name="Jon" heroName="Nerd"></Greet>
 
             <Welcome name="Bubba" heroName="Wizard"></Welcome>
-            <Welcome name="Aaron" heroName="Cool"></Welcome>
-            <Welcome name="Jon" heroName="Nerd"></Welcome> */}
+            {/* <Welcome name="Aaron" heroName="Cool"></Welcome> */}
+            {/* <Welcome name="Jon" heroName="Nerd"></Welcome> */}
 
             {/* <Hello></Hello> */}
         </div>
