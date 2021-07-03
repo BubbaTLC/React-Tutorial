dbcbf3b2db61747ffecc47a9828d76856286b1c7
BubbaTLC <chabot.tristan@gmail.com>
2021-07-03 14:48:51 -0600

    Completed video 13 Event Handling. --BC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import ClassClick from "./components/ClassClick";
+// import FunctionClick from "./components/FunctionClick";
 // import Counter from "./components/Counter";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
+// import Greet from "./components/Greet";
+// import Welcome from "./components/Welcome";
 // import Hello from "./components/Hello";
 // import Message from "./components/Message"
 
 function App() {
     return (
         <div className="App">
+            {/* <FunctionClick></FunctionClick> */}
+            <ClassClick></ClassClick>
             {/* <Counter></Counter> */}
             {/* <Message></Message> */}
             {/* <Greet name="Bubba" heroName="Wizard">
@@ -17,9 +21,9 @@ function App() {
             {/* <Greet name="Aaron" heroName="Cool"> */}
             {/* <button>Action</button> */}
             {/* </Greet> */}
-            <Greet name="Jon" heroName="Nerd"></Greet>
+            {/* <Greet name="Jon" heroName="Nerd"></Greet> */}
 
-            <Welcome name="Bubba" heroName="Wizard"></Welcome>
+            {/* <Welcome name="Bubba" heroName="Wizard"></Welcome> */}
             {/* <Welcome name="Aaron" heroName="Cool"></Welcome> */}
             {/* <Welcome name="Jon" heroName="Nerd"></Welcome> */}
 
